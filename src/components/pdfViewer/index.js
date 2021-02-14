96231cfc2eed5002f8bcbeebbdec6a019369fa7d
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-02-14 17:46:49 +0000

    checking pdf

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
+
+const isMobile = () => {
+    const mobileThreshold = 800;
+    if (typeof window !== 'undefined') {
+        if (window.innerWidth < mobileThreshold) {
+            console.log(' mobile');
+        } else {
+            console.log(' desktop');
+        }
+    }
+};
 function PdfViewer({ fileURL, closePreview }) {
-        return (
-            <>
-                <button type="button" className="overlay-close pdf-viewer-close" onClick={e => closePreview()}>
-                    Close
-                </button>
-    
-                <div className="scroll-wrapper">
-                    <iframe title="image" src={fileURL} frameBorder="0" allowFullScreen />
-                </div>
-            </>
-        );
+    isMobile();
+    return (
+        <>
+            <button type="button" className="overlay-close pdf-viewer-close" onClick={(e) => closePreview()}>
+                Close
+            </button>
+
+            <div className="scroll-wrapper">
+                <iframe title="image" src={fileURL} frameBorder="0" allowFullScreen />
+            </div>
+        </>
+    );
 }
 
 PdfViewer.defaultProps = {};
