6878d6578150fd1f3578646847f5111f853a3676
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-02-15 13:21:17 +0000

    adding pdf view on mobile

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import useDeviceDetect from "../../utils/useDeviceDetect";
-
-const isMobilePhone = () => {
-    const mobileThreshold = 800;
-    if (typeof window !== 'undefined') {
-        if (window.innerWidth < mobileThreshold) {
-            console.log(' mobile');
-        } else {
-            console.log(' desktop');
-        }
-    }
-};
+import React, { useState } from 'react';
+import useDeviceDetect from '../../utils/useDeviceDetect';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
 function PdfViewer({ fileURL, closePreview }) {
     const { isMobile } = useDeviceDetect();
-    console.log(" is mobile from hook ", isMobile);
-    if(isMobile){
-        window.open(fileURL, '_blank')
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber((prevPageNumber) => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
+    if (isMobile) {
+        return (
+            <>
+                <div className="overlay">
+                    <button
+                        type="button"
+                        className="pdf-viewer-item"
+                        className="overlay-close pdf-viewer-close"
+                        onClick={(e) => closePreview()}
+                    >
+                        Close
+                    </button>
+
+                    <Document file={fileURL} onLoadSuccess={onDocumentLoadSuccess}>
+                        <Page pageNumber={pageNumber} />
+                    </Document>
+                    <div className="controls">
+                        <button
+                            type="button"
+                            style={{ marginRight: '15px' }}
+                            disabled={pageNumber <= 1}
+                            onClick={previousPage}
+                        >
+                            Previous
+                        </button>
+                        <button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
+                            Next
+                        </button>
+                    </div>
+                </div>
+            </>
+        );
     }
     return (
         <>
