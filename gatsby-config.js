9424975b3aec886c02d2a30a02fe33c2094cd2f5
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-03-27 23:14:00 +0000

    clean up gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     siteMetadata: {
         title: 'Smart Doq',
     },
-    /* Your site config here */
     plugins: [
         'gatsby-disable-404',
         {
@@ -15,10 +14,6 @@ module.exports = {
                 },
             },
         },
-        /*
-         * Gatsby's data processing layer begins with “source”
-         * plugins. Here the site sources its data from prismic.io.
-         */
         {
             resolve: 'gatsby-source-prismic',
             options: {
@@ -26,51 +21,17 @@ module.exports = {
                 accessToken:
                     'MC5ZRVpWUWhFQUFDQUEyaEsx.au-_vR03WgVhQCbvv73vv73vv70lIXQqTO-_ve-_ve-_vX8lfu-_vXvvv71y77-977-9YO-_vQ8',
                 releaseID: 'develop',
-                linkResolver: ({ node, key, value }) => (doc) => {
-                    // Your link resolver
-                },
+                linkResolver: ({ node, key, value }) => (doc) => {},
                 fetchLinks: [],
-                htmlSerializer: ({ node, key, value }) => (type, element, content, children) => {
-                    // Your HTML serializer
-                },
+                htmlSerializer: ({ node, key, value }) => (type, element, content, children) => {},
                 schemas: {
                     blogpost: require('./src/schemas/blogpost.json'),
                 },
-
-                // Set a default language when fetching documents. The default value is
-                // '*' which will fetch all languages.
-                // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
                 lang: '*',
-
-                // Add the Prismic Toolbar script to the site. Defaults to false.
-                // Set to "legacy" if your repository requires the older toolbar script.
-                // See: https://prismic.io/docs/rest-api/beyond-the-api/the-preview-feature
                 prismicToolbar: true,
-                shouldDownloadImage: ({ node, key, value }) => {
-                  
-                },
-
-                // Provide a default set of Imgix image transformations applied to
-                // Imgix-backed gatsby-image fields. These options will override the
-                // defaults set by Prismic.
-                // See: https://docs.imgix.com/apis/url
-                imageImgixParams: {
-                    
-                },
-
-                // Provide a default set of Imgix image transformations applied to
-                // the placeholder images of Imgix-backed gatsby-image fields. These
-                // parameters will be applied over those provided in the above
-                // `imageImgixParams` option.
-                // See: https://docs.imgix.com/apis/url
-                imagePlaceholderImgixParams: {
-                   
-                },
-
-                // Set the prefix for the filename where type paths for your schemas are
-                // stored. The filename will include the MD5 hash of your schemas after
-                // the prefix.
-                // This defaults to 'prismic-typepaths---${repositoryName}'.
+                shouldDownloadImage: ({ node, key, value }) => {},
+                imageImgixParams: {},
+                imagePlaceholderImgixParams: {},
                 typePathsFilenamePrefix: 'prismic-typepaths---gatsby-source-prismic-test-site',
             },
         },
