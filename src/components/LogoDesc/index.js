2e4a2005ca79bbc426506cb3681023d0c624b6e1
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-08 19:46:28 +0100

    comit

--- src/components/LogoDesc/index.js
+++ src/components/LogoDesc/index.js
@@ -2,18 +2,12 @@ import React from 'react'
 
 const LogoDesc = ({ logo, desc, fontName }) => (
     <div id="LogoDesc" className="logoDescContainer">
-        <img className="box" src={logo}></img>
-        <div className="align-self">
-            <p className="paraLogoClassName">
-                <span
-                    className="spanLogoClassName"
-                    style={{ fontFamily: fontName }}
-                >
-                    {desc}
-                </span>
-            </p>
-        </div>
-    </div>
+    <img className="box" src={logo}></img>
+    <div
+        className="align-self"
+        dangerouslySetInnerHTML={{ __html: desc }}
+    />
+</div>
 )
 
 export default LogoDesc
